cbb0c9d079120e2f032f330bc75e56b0dca9625c
Kapil Sachdeva <ksachdeva17@gmail.com>
2024-08-30 09:09:43 -0500

    chore - remove Playground

--- src/routes/(app)/admin/+layout.svelte
+++ src/routes/(app)/admin/+layout.svelte
@@ -74,14 +74,9 @@
 				href="/workspace/documents"
 			>
 				{$i18n.t('Documents')}
-			</a>
+			</a> -->
 
-			<a
-				class="min-w-fit rounded-lg p-1.5 px-3 {$page.url.pathname.includes('/workspace/playground')
-					? 'bg-gray-50 dark:bg-gray-850'
-					: ''} transition"
-				href="/workspace/playground">{$i18n.t('Playground')}</a
-			> -->
+			
 			</div>
 		</div>
 
