e81325d473912c648c5afb48ff735f9e2d77f0aa
Kapil Sachdeva <ksachdeva17@gmail.com>
2024-09-03 16:24:43 -0500

    chore - remove the Help button and associated Modal

--- src/routes/(app)/c/[id]/+page.svelte
+++ src/routes/(app)/c/[id]/+page.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 
-	import Chat from '$lib/components/chat/Chat.svelte';
-	import Help from '$lib/components/layout/Help.svelte';
+	import Chat from '$lib/components/chat/Chat.svelte';	
 </script>
 
-<Help />
 <Chat chatIdProp={$page.params.id} />
